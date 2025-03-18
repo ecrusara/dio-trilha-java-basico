2a0c94192df1ebbebeb67a90ff9615b7d28c4475
ecrusara <ecrusara@gmail.com>
2025-03-18 17:40:00 -0300

    implementacao do projeto conforme especificação

--- ec-dio-conta-banco/src/ContaTerminal.java
+++ ec-dio-conta-banco/src/ContaTerminal.java
@@ -1,5 +1,22 @@
-public class App {
+import java.util.Scanner;
+
+public class ContaTerminal {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        Scanner scanner = new Scanner(System.in);
+        
+        System.out.println("Digite o Número da Agência!");
+        int agencia = scanner.nextInt();
+
+        System.out.println("Digite o Número da Conta:");
+        int numero = scanner.nextInt();
+        
+        System.out.println("Digite o Nome do Cliente:");
+        String nome = scanner.next();
+
+        System.out.println("Digite o Saldo da Conta:"); 
+        double saldo = scanner.nextDouble();
+
+        System.out.println(String.format("Olá %s, obrigado por criar uma conta em nosso banco, sua agência é %d, conta %d e seu saldo %.2f já está disponível para saque.", nome, agencia, numero, saldo));
+    
     }
 }
